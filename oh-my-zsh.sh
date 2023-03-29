67845576aedda8bdf54c2b30d7701d2c925f54d4
Parthis Joshi <62930148+Parthis1833@users.noreply.github.com>
2023-03-30 00:54:23 +0530

    Update oh-my-zsh.sh

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,228 +1,104 @@
-# Protect against non-zsh execution of Oh My Zsh (use POSIX syntax here)
-[ -n "$ZSH_VERSION" ] || {
-  # ANSI formatting function (\033[<code>m)
-  # 0: reset, 1: bold, 4: underline, 22: no bold, 24: no underline, 31: red, 33: yellow
-  omz_f() {
-    [ $# -gt 0 ] || return
-    IFS=";" printf "\033[%sm" $*
-  }
-  # If stdout is not a terminal ignore all formatting
-  [ -t 1 ] || omz_f() { :; }
 
-  omz_ptree() {
-    # Get process tree of the current process
-    pid=$$; pids="$pid"
-    while [ ${pid-0} -ne 1 ] && ppid=$(ps -e -o pid,ppid | awk "\$1 == $pid { print \$2 }"); do
-      pids="$pids $pid"; pid=$ppid
-    done
-
-    # Show process tree
-    case "$(uname)" in
-    Linux) ps -o ppid,pid,command -f -p $pids 2>/dev/null ;;
-    Darwin|*) ps -o ppid,pid,command -p $pids 2>/dev/null ;;
-    esac
-
-    # If ps command failed, try Busybox ps
-    [ $? -eq 0 ] || ps -o ppid,pid,comm | awk "NR == 1 || index(\"$pids\", \$2) != 0"
-  }
-
-  {
-    shell=$(ps -o pid,comm | awk "\$1 == $$ { print \$2 }")
-    printf "$(omz_f 1 31)Error:$(omz_f 22) Oh My Zsh can't be loaded from: $(omz_f 1)${shell}$(omz_f 22). "
-    printf "You need to run $(omz_f 1)zsh$(omz_f 22) instead.$(omz_f 0)\n"
-    printf "$(omz_f 33)Here's the process tree:$(omz_f 22)\n\n"
-    omz_ptree
-    printf "$(omz_f 0)\n"
-  } >&2
-
-  return 1
-}
-
-# If ZSH is not defined, use the current script's directory.
-[[ -z "$ZSH" ]] && export ZSH="${${(%):-%x}:a:h}"
-
-# Set ZSH_CACHE_DIR to the path where cache files should be created
-# or else we will use the default cache/
-if [[ -z "$ZSH_CACHE_DIR" ]]; then
-  ZSH_CACHE_DIR="$ZSH/cache"
-fi
-
-# Make sure $ZSH_CACHE_DIR is writable, otherwise use a directory in $HOME
-if [[ ! -w "$ZSH_CACHE_DIR" ]]; then
-  ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/oh-my-zsh"
-fi
-
-# Create cache and completions dir and add to $fpath
-mkdir -p "$ZSH_CACHE_DIR/completions"
-(( ${fpath[(Ie)"$ZSH_CACHE_DIR/completions"]} )) || fpath=("$ZSH_CACHE_DIR/completions" $fpath)
-
-# Check for updates on initial load...
-source "$ZSH/tools/check_for_upgrade.sh"
-
-# Initializes Oh My Zsh
-
-# add a function path
-fpath=("$ZSH/functions" "$ZSH/completions" $fpath)
-
-# Load all stock functions (from $fpath files) called below.
-autoload -U compaudit compinit zrecompile
-
-# Set ZSH_CUSTOM to the path where your custom config files
-# and plugins exists, or else we will use the default custom/
-if [[ -z "$ZSH_CUSTOM" ]]; then
-    ZSH_CUSTOM="$ZSH/custom"
-fi
-
-is_plugin() {
-  local base_dir=$1
-  local name=$2
-  builtin test -f $base_dir/plugins/$name/$name.plugin.zsh \
-    || builtin test -f $base_dir/plugins/$name/_$name
-}
-
-# Add all defined plugins to fpath. This must be done
-# before running compinit.
-for plugin ($plugins); do
-  if is_plugin "$ZSH_CUSTOM" "$plugin"; then
-    fpath=("$ZSH_CUSTOM/plugins/$plugin" $fpath)
-  elif is_plugin "$ZSH" "$plugin"; then
-    fpath=("$ZSH/plugins/$plugin" $fpath)
-  else
-    echo "[oh-my-zsh] plugin '$plugin' not found"
-  fi
-done
-
-# Figure out the SHORT hostname
-if [[ "$OSTYPE" = darwin* ]]; then
-  # macOS's $HOST changes with dhcp, etc. Use ComputerName if possible.
-  SHORT_HOST=$(scutil --get ComputerName 2>/dev/null) || SHORT_HOST="${HOST/.*/}"
-else
-  SHORT_HOST="${HOST/.*/}"
-fi
-
-# Save the location of the current completion dump file.
-if [[ -z "$ZSH_COMPDUMP" ]]; then
-  ZSH_COMPDUMP="${ZDOTDIR:-$HOME}/.zcompdump-${SHORT_HOST}-${ZSH_VERSION}"
-fi
-
-# Construct zcompdump OMZ metadata
-zcompdump_revision="#omz revision: $(builtin cd -q "$ZSH"; git rev-parse HEAD 2>/dev/null)"
-zcompdump_fpath="#omz fpath: $fpath"
-
-# Delete the zcompdump file if OMZ zcompdump metadata changed
-if ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null \
-   || ! command grep -q -Fx "$zcompdump_fpath" "$ZSH_COMPDUMP" 2>/dev/null; then
-  command rm -f "$ZSH_COMPDUMP"
-  zcompdump_refresh=1
-fi
-
-if [[ "$ZSH_DISABLE_COMPFIX" != true ]]; then
-  source "$ZSH/lib/compfix.zsh"
-  # Load only from secure directories
-  compinit -i -d "$ZSH_COMPDUMP"
-  # If completion insecurities exist, warn the user
-  handle_completion_insecurities &|
-else
-  # If the user wants it, load from all found directories
-  compinit -u -d "$ZSH_COMPDUMP"
-fi
-
-# Append zcompdump metadata if missing
-if (( $zcompdump_refresh )) \
-  || ! command grep -q -Fx "$zcompdump_revision" "$ZSH_COMPDUMP" 2>/dev/null; then
-  # Use `tee` in case the $ZSH_COMPDUMP filename is invalid, to silence the error
-  # See https://github.com/ohmyzsh/ohmyzsh/commit/dd1a7269#commitcomment-39003489
-  tee -a "$ZSH_COMPDUMP" &>/dev/null <<EOF
-
-$zcompdump_revision
-$zcompdump_fpath
-EOF
-fi
-unset zcompdump_revision zcompdump_fpath zcompdump_refresh
-
-# zcompile the completion dump file if the .zwc is older or missing.
-if command mkdir "${ZSH_COMPDUMP}.lock" 2>/dev/null; then
-  zrecompile -q -p "$ZSH_COMPDUMP"
-  command rm -rf "$ZSH_COMPDUMP.zwc.old" "${ZSH_COMPDUMP}.lock" 
-fi
-
-# Load all of the config files in ~/oh-my-zsh that end in .zsh
-# TIP: Add files you don't want in git to .gitignore
-for config_file ("$ZSH"/lib/*.zsh); do
-  custom_config_file="$ZSH_CUSTOM/lib/${config_file:t}"
-  [[ -f "$custom_config_file" ]] && config_file="$custom_config_file"
-  source "$config_file"
-done
-unset custom_config_file
-
-# Load all of the plugins that were defined in ~/.zshrc
-for plugin ($plugins); do
-  if [[ -f "$ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-    source "$ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh"
-  elif [[ -f "$ZSH/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-    source "$ZSH/plugins/$plugin/$plugin.plugin.zsh"
-  fi
-done
-unset plugin
-
-# Load all of your custom configurations from custom/
-for config_file ("$ZSH_CUSTOM"/*.zsh(N)); do
-  source "$config_file"
-done
-unset config_file
-
-# Load the theme
-is_theme() {
-  local base_dir=$1
-  local name=$2
-  builtin test -f $base_dir/$name.zsh-theme
-}
-
-if [[ -n "$ZSH_THEME" ]]; then
-  if is_theme "$ZSH_CUSTOM" "$ZSH_THEME"; then
-    source "$ZSH_CUSTOM/$ZSH_THEME.zsh-theme"
-  elif is_theme "$ZSH_CUSTOM/themes" "$ZSH_THEME"; then
-    source "$ZSH_CUSTOM/themes/$ZSH_THEME.zsh-theme"
-  elif is_theme "$ZSH/themes" "$ZSH_THEME"; then
-    source "$ZSH/themes/$ZSH_THEME.zsh-theme"
-  else
-    echo "[oh-my-zsh] theme '$ZSH_THEME' not found"
-  fi
-fi
-
-#alias for this profile
+# Aliases
 alias shortcut='code ~/.oh-my-zsh'
-
-#short cut for cd
 alias ..='cd ..'
-
-#refres source ~/.zshrc
-
-#alias for power off
+alias refresh='source ~/.zshrc'
 alias poweroff='sudo /sbin/shutdown -h now'
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade'
+alias boost='sudo rm -rf /var/log/'
+alias clean='rm -rf ~/.local/share/Trash/*'
+alias sysupdate='sudo apt update && sudo apt upgrade'
 
- # Get OS X Software Updates, update Homebrew itself, and upgrade installed Homebrew packages
-alias update="sudo softwareupdate -i -a; brew update; brew upgrade"
+# Shortcuts for navigating directories
+alias home='cd ~'
+alias desktop='cd ~/Desktop'
+alias downloads='cd ~/Downloads'
+alias documents='cd ~/Documents'
+alias music='cd ~/Music'
+alias pictures='cd ~/Pictures'
+alias videos='cd ~/Videos'
+
+# Shortcuts for frequently used commands
+alias ls='ls -GFh' # colorize output and show file sizes in human-readable format
+alias ll='ls -alh' # list all files (including hidden ones) in long format
+alias grep='grep --color=auto' # colorize grep output
+alias top='top -o cpu' # sort processes by CPU usage
+alias htop='sudo htop' # launch htop with sudo privileges
+
+# Other aliases
+alias editzsh='code ~/.zshrc' # open .zshrc file in Visual Studio Code
+alias refreshzsh='source ~/.zshrc' # refresh .zshrc file without restarting the terminal
+alias please='sudo' # add "please" before a command to run it with sudo privileges (e.g. "please rm -rf /")
+
+# Shortcuts for Git commands
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gco='git checkout'
+alias gb='git branch'
+alias glog='git log --oneline --graph'
+
+#application 
+
+# Open Visual Studio Code
+alias code='code-insiders'
+
+# Open Google Chrome
+alias chrome='google-chrome-stable'
+
+# Open Firefox Developer Edition
+alias firefoxdev='firefox-developer-edition'
+
+# Open VLC Media Player
+alias vlc='vlc -I ncurses'
+
+# Open Spotify
+alias spotify='flatpak run com.spotify.Client'
+
+# Open Telegram Desktop
+alias telegram='telegram-desktop'
+
+# Open Slack
+alias slack='slack'
+
+# Open Discord
+alias discord='discord'
 
 
-# Speed-up Terminal load time by clearing system logs
-alias boost="sudo rm -rf /var/log/"
+# Shortcuts for network commands
+alias myip='curl ipinfo.io/ip' # get your public IP address
+alias ports='lsof -i -P -n | grep LISTEN' # list all open ports
 
- #clean trust 
-alias clean="sudo rm -rf /tmp/"
+# Shortcuts for file manipulation commands
+alias showhidden='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder' # show hidden files in Finder
+alias hidehidden='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder' # hide hidden files in Finder
+alias showdesktop='defaults write com.apple.finder CreateDesktop true; killall Finder' # show desktop icons
+alias hidedesktop='defaults write com.apple.finder CreateDesktop false; killall Finder' # hide desktop icons
 
+# Other aliases
+alias cls='clear' # clear the terminal screen
+alias py='python' # shortcut for running Python interpreter
+alias py2='python2' # shortcut for running Python 2 interpreter
+alias py3='python3' # shortcut for running Python 3 interpreter
+alias jn='jupyter notebook' # shortcut for launching Jupyter Notebook
 
-export ANDROID_HOME="/home/parthis/Android/Sdk/"
-export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
+# Flutter aliases
+alias flutter='~/flutter/bin/flutter' # replace with your Flutter installation directory
+alias fpub='flutter pub' # shortcut for running 'flutter pub' commands
+alias fclean='flutter clean' # shortcut for running 'flutter clean'
+alias fbuild='flutter build' # shortcut for running 'flutter build'
+alias frun='flutter run' # shortcut for running 'flutter run'
+alias ftest='flutter test' # shortcut for running 'flutter test'
+alias fformat='flutter format .' # shortcut for running 'flutter format' on current directory
+alias fdoctor='flutter doctor' # shortcut for running 'flutter doctor'
 
-export JAVA_HOME="/home/parthis/.sdkman/candidates/java/current"
-
+# Environment variables
+export ANDROID_HOME="$HOME/Android/Sdk"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools"
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
 export PATH="$PATH:/usr/lib/dart/bin"
+export M2_HOME="/opt/apache-maven-3.6.1"
+export PATH="$PATH:$M2_HOME/bin"
 
-
-export M2_HOME="$PATH:/opt/apache-maven-3.6.1"
-export PATH="$PATH:$M2_HOME/bin:$PATH"
-
-
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+# Initialize SDKMAN
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
